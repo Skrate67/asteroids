2599768ae5ef7900bad3afae8bea24506f27c2fa
Skrate67 <liamalancooney@gmail.com>
2025-10-18 21:02:35 -0400

    10/18 progress

---
+++ asteroid.py
@@ -0,0 +1,13 @@
+import pygame # type: ignore
+from circleshape import CircleShape
+from constants import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, ASTEROID_COLOR, self.position, self.radius, ASTEROID_LINE_WIDTH)
+        
+    def update(self, dt):
+        self.position += (self.velocity * dt)
--- main.py
+++ main.py
@@ -1,17 +1,52 @@
-import pygame
+import pygame # type: ignore
+import sys
 from constants import *
+from player import *
+from asteroid import *
+from asteroidfield import *
+from shot import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt: int = 0
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    bullets = pygame.sprite.Group()
+    Player.containers = (updateable, drawable)
+    Asteroid.containers = (updateable, drawable, asteroids)
+    AsteroidField.containers = (updateable)
+    Shot.containers = (updateable, drawable, bullets)
+
     print(f"""
 Starting Asteroids!
 Screen width: {SCREEN_WIDTH}
 Screen height: {SCREEN_HEIGHT}
         """)
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    screen = pygame.display.set_mode(SCREEN_SIZE)
+    player = Player(*PLAYER_SPAWN_POINT)
+    asteroid_field = AsteroidField()
     while True:
-        screen.fill((0, 0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+            
+        dt = clock.tick(FPS) / 1000
+            
+        screen.fill(SCREEN_COLOR)
+        
+        updateable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        for asteroid in asteroids:
+            if asteroid.check_collisions(player):
+                print("Game Over!")
+                sys.exit()
+
         pygame.display.flip()
+        
 
 if __name__ == "__main__":
     main()
